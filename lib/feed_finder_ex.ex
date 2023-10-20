e76ae62a74212ec1286b2d6337a9a491405287d3
DesastreNatural <link89mb@gmail.com>
2023-10-20 12:32:45 +0200

    initial commit

---
+++ lib/feed_finder_ex.ex
@@ -0,0 +1,18 @@
+defmodule FeedFinderEx do
+  @moduledoc """
+  Documentation for `FeedFinderEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FeedFinderEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/feed_finder_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule FeedFinderExTest do
+  use ExUnit.Case
+  doctest FeedFinderEx
+
+  test "greets the world" do
+    assert FeedFinderEx.hello() == :world
+  end
+end
