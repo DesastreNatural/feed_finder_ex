4e760c42b9dcd9ddbc279dfd811ed68b71aeaa78
DesastreNatural <link89mb@gmail.com>
2023-10-23 17:06:54 +0200

    modified to return tuple

--- native/feed_finder_ex/src/lib.rs
+++ native/feed_finder_ex/src/lib.rs
@@ -1,13 +1,27 @@
+use std::collections::HashMap;
+
 use feedfinder::{Feed, detect_feeds};
 use feedfinder::Url;
 
+fn encode_feed(feed: &Feed) -> HashMap<String, String> {
+    let mut encoded: HashMap<String, String> = HashMap::new();
+    encoded.insert("url".to_string(),feed.url().to_string());
+    encoded.insert("feed_type".to_string(),format!("{:?}",feed.feed_type()));
+    encoded.insert("title".to_string(),feed.title().unwrap_or("").to_string());
+    return encoded;
+}
+
 #[rustler::nif]
-fn find_feeds(url: String, html_data: String) -> Vec<String> {
-    let p_url = Url::parse(&url).expect("unable to parse url");
-    let void: Vec<String> = Vec::new();
-    match detect_feeds(&p_url,&html_data) {
-        Ok(feeds) => { return feeds.iter().map(|x| x.url().to_string()).collect(); }
-        Err(_err) => { return void; }
+fn find_feeds(url: String, html_data: String) -> Result<Vec<HashMap<String, String>>,String> {
+    let p_url = Url::parse(&url);
+    match p_url {
+        Ok(url) => {
+            match detect_feeds(&url,&html_data) {
+                Ok(feeds) => { return Ok(feeds.iter().map(|x| encode_feed(x)).collect()); }
+                Err(_err) => { return Err(format!("{:?}",_err)); }
+            }
+        }
+        Err(_err) => { return Err(format!("{:?}",_err)); }
     }
 }
 
--- test/feed_finder_ex_test.exs
+++ test/feed_finder_ex_test.exs
@@ -2,11 +2,28 @@ defmodule FeedFinderExTest do
   use ExUnit.Case
   doctest FeedFinderEx
 
-  test "greets the world" do
+  test "basic test" do
+    url = "https://example.com/example"
+    html_fake_data = """
+      <html>
+          <head>
+              <title>Example</title>
+              <link rel="alternate" href="/posts.rss" type="application/rss+xml" />
+          </head>
+          <body>
+              My fun page with a feed.
+          </body>
+      </html>
+    """
+    test_data = FeedFinderEx.find_feeds(url,html_fake_data)
+    assert  test_data == {:ok,[%{"feed_type" => "Rss","title" => "", "url" => "https://example.com/posts.rss"}]}
+  end
+
+  test "test with NYT" do
     url = "https://www.nytimes.com/"
     {:ok, response} = HTTPoison.get(url)
     test_data = FeedFinderEx.find_feeds(url,response.body)
-    IO.inspect test_data
-    assert  test_data == :world
+    assert  test_data == {:ok,[%{"feed_type" => "Rss","title" => "RSS", "url" => "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"}]}
   end
+
 end
